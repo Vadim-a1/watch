0108c6d068eaaab3ba0b11e003245d4a18452fc8
VADIM <xxxvadimxxx25@list.ru>
2025-09-22 21:50:37 +0300

    test

--- WinFormsApp1/Form2.cs
+++ WinFormsApp1/Form2.cs
@@ -24,6 +24,51 @@ namespace WinFormsApp1
 
         private void button2_Click(object sender, EventArgs e)
         {
+            button2.FlatAppearance.BorderSize = 0;
+            button2.BackColor = Color.Transparent;
+            button2.FlatAppearance.MouseOverBackColor = Color.Transparent; // Прозрачный фон при наведении
+            button2.FlatAppearance.MouseDownBackColor = Color.Transparent; // Прозрачный фон при нажатии
+
+            button2.FlatStyle = FlatStyle.Flat; // Плоский стиль кнопки
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.FlatAppearance.BorderSize = 0;
+            button1.BackColor = Color.Transparent;
+            button1.FlatAppearance.MouseOverBackColor = Color.Transparent; // Прозрачный фон при наведении
+            button1.FlatAppearance.MouseDownBackColor = Color.Transparent; // Прозрачный фон при нажатии
+            
+            button1.FlatStyle = FlatStyle.Flat; // Плоский стиль кнопки
+
+            Form3 f3 = new Form3();   // создаём вторую форму
+            f3.Show();                // показываем её
+            this.Hide();              // скрываем текущую (Form1)
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
 
         }
     }
